5d922ee6c8ca1342e70ac0e790501d24915faaca
Jay-Man-Web <pfunzojohn06@gmail.com>
2026-04-02 22:05:41 +0200

    Add files via upload

---
+++ scripts/houses-section.js
@@ -0,0 +1,82 @@
+let houses = [{
+  "label" : "Hotel in Cape Town City Centre",
+  "type" : "house",
+  "thumbnalID" : "0001",
+  "priceCents" : 2768,
+  "rating" : 4.5,
+  "location" : "johannesburg",
+  "description" : "",
+  "bedrooms" : 1,
+  "guests" : 2,
+  "bathrooms" : 1,
+  "offers" : ["Wifi", "Dedicated workspace", "Pool", "Air Conditioning", "Free parking"]
+},
+{
+  "label" : "Hotel in Cape Town City Centre",
+  "type" : "house",
+  "thumbnalID" : "0001",
+  "priceCents" : 2768,
+  "rating" : 4.5,
+  "location" : "johannesburg",
+  "description" : "",
+  "bedrooms" : 1,
+  "guests" : 2,
+  "bathrooms" : 1,
+  "offers" : ["Wifi", "Dedicated workspace", "Pool", "Air Conditioning", "Free parking"]
+},
+{
+  "label" : "Hotel in Cape Town City Centre",
+  "type" : "house",
+  "thumbnalID" : "0001",
+  "priceCents" : 2768,
+  "rating" : 4.5,
+  "location" : "johannesburg",
+  "description" : "",
+  "bedrooms" : 1,
+  "guests" : 2,
+  "bathrooms" : 1,
+  "offers" : ["Wifi", "Dedicated workspace", "Pool", "Air Conditioning", "Free parking"]
+},
+{
+  "label" : "Hotel in Cape Town City Centre",
+  "type" : "house",
+  "thumbnalID" : "0001",
+  "priceCents" : 2768,
+  "rating" : 4.5,
+  "location" : "johannesburg",
+  "description" : "",
+  "bedrooms" : 1,
+  "guests" : 2,
+  "bathrooms" : 1,
+  "offers" : ["Wifi", "Dedicated workspace", "Pool", "Air Conditioning", "Free parking"]
+},
+{
+  "label" : "Hotel in Cape Town City Centre",
+  "type" : "house",
+  "thumbnalID" : "0001",
+  "priceCents" : 2768,
+  "rating" : 4.5,
+  "location" : "johannesburg",
+  "description" : "",
+  "bedrooms" : 1,
+  "guests" : 2,
+  "bathrooms" : 1,
+  "offers" : ["Wifi", "Dedicated workspace", "Pool", "Air Conditioning", "Free parking"]
+},
+{
+  "label" : "Hotel in Cape Town City Centre",
+  "type" : "house",
+  "thumbnalID" : "0001",
+  "priceCents" : 2768,
+  "rating" : 4.5,
+  "location" : "johannesburg",
+  "description" : "",
+  "bedrooms" : 1,
+  "guests" : 2,
+  "bathrooms" : 1,
+  "offers" : ["Wifi", "Dedicated workspace", "Pool", "Air Conditioning", "Free parking"]
+}];
+
+function contentGenerate(contentField) {
+  
+}
