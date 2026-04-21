e66b0204c93cb1f9bca30b28291924374eb1e474
Jay-Man-Web <pfunzojohn06@gmail.com>
2026-04-21 05:16:43 +0200

    update  fix

--- scripts/houses-section.js
+++ scripts/houses-section.js
@@ -1,55 +1,3 @@
-/*
-import {houses} from "../scripts/houses.js";
-
-function generateCard(house) {
-  return `
-    <a href="../pages/home-details.html?id=${house.id}" class="property-container" role="button" onclick="viewHouse(${house.id})">
-      <div class="property-thumbnail-container">
-        <img class="property-thumbnail" src="../images/home-thumbnails/${house.thumbnailID}.png" alt="no image">
-      </div>
-      <div class="property-details-container">
-        <p class="property-label">${house.label}</p>
-        <p class="property-price-rating">
-          R${(house.priceCents / 100).toFixed(2)} for 2 nights <br/> ★ ${house.rating}
-        </p>
-      </div>
-    </a>
-  `;
-};
-
-let popularHTML = '';
-let newHTML = '';
-let budgetFriendlyHTML = '';
-
-houses.forEach(house => {
-  if (house.rating >= 4) {
-    popularHTML += generateCard(house);
-  }
-
-  newHTML += generateCard(house);
-
-  if (house.priceCents <= 150000) {
-    budgetFriendlyHTML += generateCard(house);
-  }
-  
-});
-
-document.getElementById('popular').innerHTML = popularHTML;
-document.getElementById('new').innerHTML = newHTML;
-document.getElementById('budget-friendly').innerHTML = budgetFriendlyHTML;
-
-
-function viewHouse(id) {
-  // Redirect to new page with id in URL
-  window.location.href = `../pages/home-details.html?id=${id}`;
-}
-*/
-
-
-
-/* =========================
-   houses-list.js
-   ========================= */
 import { houses } from "../scripts/houses.js";
 import { formatPrice } from "./utils.js";
 
@@ -62,7 +10,7 @@ function generateCard(house) {
     <div class="property-container" data-id="${house.id}">
       <div class="property-thumbnail-container">
         <img class="property-thumbnail" 
-             src="../images/home-thumbnails/${house.thumbnailID}.png" 
+             src="images/home-thumbnails/${house.thumbnailID}.png" 
              alt="${house.label}">
       </div>
       <div class="property-details-container">
@@ -98,7 +46,7 @@ document.addEventListener("click", (e) => {
   if (!card) return;
 
   const id = card.dataset.id;
-  window.location.href = `../pages/home-details.html?id=${id}`;
+  window.location.href = `pages/home-details.html?id=${id}`;
 });
 
 render();
